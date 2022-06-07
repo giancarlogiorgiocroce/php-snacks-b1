e5c438b472f6504056abd7068226a487bb39bb30
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-07 15:43:54 +0200

    Snack 2 rifatto e completato

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,18 +1,19 @@
 <?php
 /*
 ## Snack 2
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-## Snack 5
-Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
-aggiungete poi i seguenti snack da realizzare
-## Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60
-## Snack 4
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
-## Snack 7
-Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+Passare come parametri GET name, mail e age 
+e verificare che name sia più lungo di 3 caratteri, 
+che mail contenga un punto e una chiocciola 
+e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+
+    <h1><?php if(!$isPermitted){ echo "<h2>Accesso negato</h2>"} ?></h1>
+
 */
+
+// var_dump($_GET);
+
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +25,17 @@ Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno 
     <title>Document</title>
 </head>
 <body>
-    <h1>HW</h1>
+
+    <h1>Controllo in corso...</h1>
+    <h2>
+        <?php
+            if(strlen($_GET['name']) > 3 && strpos($_GET['email'], '@') && strpos($_GET['email'], '.') && is_numeric($_GET['age'])){
+                echo 'Accesso consentito';
+            }else{
+                echo 'Accesso negato';
+            };
+        ?>
+    </h2>
+  
 </body>
 </html>
