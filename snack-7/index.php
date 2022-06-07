347be60ec0297d5d3d224b94d4f18eb1d24c05ad
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-07 16:54:40 +0200

    Scritto array Snack 7

--- snack-7/index.php
+++ snack-7/index.php
@@ -1,18 +1,30 @@
 <?php
 /*
-## Snack 2
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-## Snack 5
-Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
-aggiungete poi i seguenti snack da realizzare
-## Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60
-## Snack 4
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
 ## Snack 7
-Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno.
 */
+
+$classe = [
+    [
+        'nome' => 'Ciccio',
+        'cognome' => 'Pasticcio',
+        'voti' => [5, 6, 7, 5, 5, 2],
+    ],
+    [
+        'nome' => 'Braccobaldo',
+        'cognome' => 'Bau',
+        'voti' => [5, 6, 8, 8, 10, 9],
+    ],
+    [
+        'nome' => 'Tonino',
+        'cognome' => 'Latorre',
+        'voti' => [1, 7, 8, 5, 5, 2],
+    ],
+];
+
+var_dump($classe);
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +36,13 @@ Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno 
     <title>Document</title>
 </head>
 <body>
-    <h1>HW</h1>
+    <h1>HW7</h1>
+    <ul>
+        <?php
+            foreach($classe as $studente => $nome){
+                echo "$studente = $nome <br>";
+            }
+        ?>
+    </ul>
 </body>
 </html>
